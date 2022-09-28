8dd6699bb3294afc60128dd7486a31701e8c6996
Diogo Henrique <diogohs110@gmail.com>
2022-09-28 20:11:07 -0300

    JS and Design updated

--- app.js
+++ app.js
@@ -1,39 +1,60 @@
-const rato = document.getElementById('rato');
-const body = document.body;
-const errou = document.getElementsByClassName('errou')[0];
-const pegou = document.getElementsByClassName('pegou')[0];
+const rat = document.getElementById('rat');
+const btnControl = document.getElementsByClassName('btn-control')[0];
+const catchScore = document.getElementsByClassName('catch')[0];
+const missScore = document.getElementsByClassName('miss')[0];
+let btnOnStart = true;
+let id;
+let catchCount = 0;
+let missCount = -1;
 
-let isMoving = true;
-let cont = 0;
-let erro = 0;
+btnControl.addEventListener('click', click);
+rat.addEventListener('click', gotCatch);
+document.body.addEventListener('click', bodyClick);
 
-rato.addEventListener('click', foiPego);
-window.addEventListener('load', correr);
-body.addEventListener('click', naoPegou);
-
-function correr() {
-	if (isMoving) {
-		rato.style.top = randomNumber();
-		rato.style.right = randomNumber();
-		setTimeout(correr, 600);
+function click() {
+	if (btnOnStart) {
+		start();
+		btnControl.innerHTML = 'PAUSE';
+		btnOnStart = false;
+	} else {
+		pause();
+		btnControl.innerHTML = 'START';
+		btnOnStart = true;
 	}
 }
 
-function randomNumber() {
-	return Math.floor(Math.random() * 88) + '%';
+function start() {
+	id = setInterval(run, 600);
 }
 
-function foiPego() {
-	isMoving = false;
-	cont++;
-	pegou.innerHTML = cont;
-	setTimeout(function () {
-		isMoving = true;
-		correr();
-	}, 1500);
+function pause() {
+	clearInterval(id);
 }
 
-function naoPegou() {
-	erro++;
-	errou.innerHTML = erro;
+function run() {
+	rat.style.top = randomPos();
+	rat.style.right = randomPos();
 }
+
+function gotCatch() {
+	if (!btnOnStart) {
+		catchCount++;
+		catchScore.innerHTML = `${catchCount}`;
+		document.body.classList.add('catched');
+		pause();
+
+		setTimeout(function () {
+			document.body.classList.remove('catched');
+			start();
+		}, 800);
+	}
+}
+
+function bodyClick() {
+	if (!btnOnStart) {
+		missCount++;
+		missScore.innerHTML = `${missCount}`;
+	}
+}
+
+const randomPos = () => Math.floor(Math.random() * 90) + '%';
